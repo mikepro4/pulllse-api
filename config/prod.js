ff00c10e390137d9d54e6aa361abeb2f3ec89c0c
anmst21 <anmstudios21c@gmail.com>
2023-10-09 22:55:25 -0400

    prod test

---
+++ config/prod.js
@@ -0,0 +1,9 @@
+module.exports = {
+  mongoURI: process.env.MONGO_URI,
+  // cookieKey: process.env.COOKIE_KEY,
+  // sgKey: process.env.SG_KEY,
+  // myEmail: process.env.MY_EMAIL,
+  awsAccessSecret: process.env.AWS_ACCESS_SECRET,
+  awsAccessId: process.env.AWS_ACCESS_ID,
+  // awsRegion: process.env.AWS_REGION,
+};
